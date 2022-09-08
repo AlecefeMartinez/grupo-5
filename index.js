0568ed9b67c96f2c908d450be344c9e26f3dfb1f
AlecefeMartinez <104871029+AlecefeMartinez@users.noreply.github.com>
2022-09-08 19:30:23 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res)=>{
+    res.send('Hello')
+});
+
+app.listen(5000, () => {
+    console.log('Server is running')
+});
